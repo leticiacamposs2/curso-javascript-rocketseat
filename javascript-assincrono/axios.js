eaa1554d8e98df5e123a568e3a6ad7d5fa9d05aa
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-25 20:16:04 -0300

    utilizando axios

---
+++ javascript-assincrono/axios.js
@@ -0,0 +1,9 @@
+console.log('----------- AXIOS -----------');
+
+axios.get('https://api.github.com/users/diego3g')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.warn(error);
+    });
